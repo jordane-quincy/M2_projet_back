7ce283f376b8fcf51a3e4b51352b327bc4163db6
CANONNE Jordan <Jordan.Canonne@etu.univ-valenciennes.fr>
2017-06-06 12:51:43 +0200

    Controller

--- SKE/src/main/java/org/istv/ske/core/TestController.java
+++ SKE/src/main/java/org/istv/ske/core/TestController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest")
 public class TestController {
 
 	@RequestMapping("/player/{name}")
 	public String getAccountsByEmail(@PathVariable("name") String name) {
 		return name;
-	} 
+	}
+	
+	@RequestMapping("/client")
+	public String getClient(int id){
+		return "test";
+	}
 
 
 }
