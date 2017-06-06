75f4911b222cf4fa15b4f334ac94b9fa5ed5a3e6
Assane <sarrassane92@yahoo.fr>
2017-06-06 13:21:03 +0200

    feat(entities): update entities

--- SKE/src/main/java/org/istv/ske/dal/Formation.java
+++ SKE/src/main/java/org/istv/ske/dal/Formation.java
@@ -3,12 +3,14 @@ package org.istv.ske.dal;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-import lombok.Data;
 
 @Entity
-@Data
+
+@Table(name = "formation")
 public class Formation {
+	
 	
 	@Id
 	@GeneratedValue
@@ -16,7 +18,38 @@ public class Formation {
 	private String formationLevel;
 	private String formationName;
 
-	
+	public int getIdFormation() {
+		return idFormation;
+	}
+	public void setIdFormation(int idFormation) {
+		this.idFormation = idFormation;
+	}
+	public String getFormationLevel() {
+		return formationLevel;
+	}
+	public void setFormationLevel(String formationLevel) {
+		this.formationLevel = formationLevel;
+	}
+	public String getFormationName() {
+		return formationName;
+	}
+	public void setFormationName(String formationName) {
+		this.formationName = formationName;
+	}
+	@Override
+	public String toString() {
+		return "Formation [idFormation=" + idFormation + ", formationLevel=" + formationLevel + ", formationName="
+				+ formationName + "]";
+	}
+	public Formation(int idFormation, String formationLevel, String formationName) {
+		super();
+		this.idFormation = idFormation;
+		this.formationLevel = formationLevel;
+		this.formationName = formationName;
+	}
+	public Formation() {
+		super();
+	}
 	
 	
 
