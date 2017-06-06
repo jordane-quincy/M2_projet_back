95874b924ace55b719ac8eaeb770e4a916b3abb5
Samir <samir.moutawakil59@gmail.com>
2017-06-06 12:58:34 +0200

    Auto stash before merge of "master" and "origin/master"

--- SKE/src/main/java/org/istv/ske/core/TestController.java
+++ SKE/src/main/java/org/istv/ske/core/TestController.java
@@ -2,14 +2,18 @@ package org.istv.ske.core;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-	@RequestMapping("/player/{name}")
-	public String getAccountsByEmail(@PathVariable("name") String name) {
-		return name;
+	@RequestMapping(value = { "/player" }, method = RequestMethod.GET, headers = "Accept=*/*", produces = "Application/json")
+	@ResponseBody
+	public String getAccountsByEmail() {
+		String s = "[{\"Nom\": \"test\"},{\"Type\": \"\"}]";
+		return s;
 	} 
 
 
