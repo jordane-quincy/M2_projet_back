3ccba48c75ee8acbd461bd2b4d47ce6832483f42
Wastamane <duthoit501@gmail.com>
2017-06-06 15:55:30 +0200

    Offer SI

---
+++ SKE/src/main/java/org/istv/ske/core/service/OfferServiceImlp.java
@@ -0,0 +1,34 @@
+package org.istv.ske.core.service;
+
+import java.util.List;
+
+import org.istv.ske.dal.Offer;
+import org.istv.ske.dal.User;
+
+public class OfferServiceImlp implements OfferService {
+
+	@Override
+	public Offer createOffer(User idUser, String titleOffer, int duration, String descriptionOffer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteOffer(int idOffer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Offer updateOffer(Offer offer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Offer> getAll(int idUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
