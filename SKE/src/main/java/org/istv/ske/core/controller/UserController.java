0806ac1e8027ba51ff080cc4fa0e39e8f9be2de5
CANONNE Jordan <Jordan.Canonne@etu.univ-valenciennes.fr>
2017-06-06 13:58:25 +0200

    create UserController

--- SKE/src/main/java/org/istv/ske/core/controller/UserController.java
+++ SKE/src/main/java/org/istv/ske/core/controller/UserController.java
@@ -6,5 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+	
+	
+	@RequestMapping(value = "/create", method = RequestMethode.POST)
 }
