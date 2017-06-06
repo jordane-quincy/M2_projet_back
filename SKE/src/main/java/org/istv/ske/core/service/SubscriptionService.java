41ad30cc322c432d1c8ad2b6d4bfa405ff6ae143
CANONNE Jordan <Jordan.Canonne@etu.univ-valenciennes.fr>
2017-06-06 16:14:58 +0200

    ajout de l'annotation  @Service sur les impplémentations

--- SKE/src/main/java/org/istv/ske/core/service/SubscriptionService.java
+++ SKE/src/main/java/org/istv/ske/core/service/SubscriptionService.java
@@ -3,7 +3,7 @@ package org.istv.ske.core.service;
 import org.istv.ske.dal.Appointment;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface SubscriptionService {
 
 	public boolean subscription(Appointment a);
