c4ba8560c7a036b296af23b5103f98cf154a763a
Samir <samir.moutawakil59@gmail.com>
2017-06-07 18:11:55 +0200

    @Service sur accountService

--- SKE/src/main/java/org/istv/ske/dal/service/AccountCertificationService.java
+++ SKE/src/main/java/org/istv/ske/dal/service/AccountCertificationService.java
@@ -2,6 +2,7 @@ package org.istv.ske.dal.service;
 
 import org.istv.ske.dal.entities.User;
 
+
 public interface AccountCertificationService {
 	public User activate(String token);
 	public boolean isActivated(long userID);
