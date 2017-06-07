eaa8a3feb4fc58cf924990a4cc4fa9ef2d2f4fda
MaD <maximdegres@gmail.com>
2017-06-07 08:54:14 +0200

    double liaison table

--- SKE/src/main/java/org/istv/ske/dal/Formation.java
+++ SKE/src/main/java/org/istv/ske/dal/Formation.java
@@ -5,59 +5,61 @@ import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
 
 @Entity
 @Table(name = "formation")
 public class Formation {
-	
-	
+
 	@Id
 	@GeneratedValue
 	private int idFormation;
 	private String formationLevel;
 	private String formationName;
-	
-	@OneToMany(mappedBy="formation")
+
+	@OneToMany(mappedBy = "formation")
 	private Collection<User> users;
-	
-	
+
 	public int getIdFormation() {
 		return idFormation;
 	}
+
 	public void setIdFormation(int idFormation) {
 		this.idFormation = idFormation;
 	}
+
 	public String getFormationLevel() {
 		return formationLevel;
 	}
+
 	public void setFormationLevel(String formationLevel) {
 		this.formationLevel = formationLevel;
 	}
+
 	public String getFormationName() {
 		return formationName;
 	}
+
 	public void setFormationName(String formationName) {
 		this.formationName = formationName;
 	}
+
 	@Override
 	public String toString() {
 		return "Formation [idFormation=" + idFormation + ", formationLevel=" + formationLevel + ", formationName="
 				+ formationName + "]";
 	}
+
 	public Formation(int idFormation, String formationLevel, String formationName) {
 		super();
 		this.idFormation = idFormation;
 		this.formationLevel = formationLevel;
 		this.formationName = formationName;
 	}
+
 	public Formation() {
 		super();
 	}
-	
-	
 
 }
--- SKE/src/main/java/org/istv/ske/dal/Subject.java
+++ SKE/src/main/java/org/istv/ske/dal/Subject.java
@@ -4,25 +4,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
 
 @Entity
 @Table(name = "subject")
 public class Subject {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int subjectId;
 	private String subjectName;
-	
+
 	@ManyToOne
-	private Domain domain;	
-	
-	public Subject(){
-		
+	private Domain domain;
+
+	public Subject() {
+
 	}
 
 	public int getSubjectId() {
