7bffe756513baa21fb3aafeed739fe166c9dc20b
clemscode <ruffin.cle@gmail.com>
2017-06-07 14:20:51 +0200

    appointment

--- SKE/src/main/java/org/istv/ske/core/service/AppointmentServiceImpl.java
+++ SKE/src/main/java/org/istv/ske/core/service/AppointmentServiceImpl.java
@@ -1,25 +1,29 @@
 package org.istv.ske.core.service;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
-import org.istv.ske.dal.Appointment;
-import org.istv.ske.dal.Offer;
-import org.istv.ske.dal.User;
+import org.istv.ske.dal.entities.Appointment;
+import org.istv.ske.dal.entities.Offer;
+import org.istv.ske.dal.entities.User;
 import org.istv.ske.dal.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class AppointmentServiceImpl implements AppointmentService{
-AppointmentRepository appointmentRepository ;
+@Service
+public class AppointmentServiceImpl implements AppointmentService {
+	
+	@Autowired
+	private AppointmentRepository appointmentRepository ;
+
 	@Override
 	public Appointment createAppointment(Offer offer, User applicant, Date date) {
-	Appointment appointment = new Appointment();
-	appointment.setOffer(offer);
-	appointment.setApplicant(applicant);
-	appointment.setDate(date);
-	appointmentRepository.save(appointment);
+		Appointment appointment = new Appointment();
+		appointment.setOffer(offer);
+		appointment.setApplicant(applicant);
+		appointment.setDate(date);
+		appointmentRepository.save(appointment);
 		return appointment;
 	}
 
@@ -30,20 +34,21 @@ AppointmentRepository appointmentRepository ;
 
 	@Override
 	public Appointment updateAppointment(Appointment appointment, Offer offer, User applicant, Date date) {
-		Appointment appointmentFound =  appointmentRepository.findOne( appointment.getId().intValue());
-		 
-		  appointmentFound.setApplicant(applicant);
-		  appointmentFound.setOffer(offer);
-		  appointmentFound.setDate(date);
-		  appointmentRepository.save(appointmentFound);
+		Appointment appointmentFound =  appointmentRepository.findOne(appointment.getId());
+		appointmentFound.setApplicant(applicant);
+		appointmentFound.setOffer(offer);
+		appointmentFound.setDate(date);
+		appointmentRepository.save(appointmentFound);
 		return appointmentFound;
 	}
 
 	@Override
 	public List<Appointment> getAll() {
-		
-		return  (List<Appointment>) appointmentRepository.findAll();
-	
+		List<Appointment> users = new ArrayList<>();
+		for(Appointment u : appointmentRepository.findAll()) {
+			users.add(u);
+		}
+		return users;
 	}
 
 }
