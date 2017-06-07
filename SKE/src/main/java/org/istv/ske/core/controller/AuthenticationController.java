f807ce6621462681bb99165f460beb56b637c0ed
clemscode <ruffin.cle@gmail.com>
2017-06-07 09:03:59 +0200

    JsonService

--- SKE/src/main/java/org/istv/ske/core/controller/AuthenticationController.java
+++ SKE/src/main/java/org/istv/ske/core/controller/AuthenticationController.java
@@ -4,32 +4,61 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.istv.ske.core.exception.BadRequestException;
 import org.istv.ske.core.service.AuthenticationService;
+import org.istv.ske.core.service.JsonService;
+import org.istv.ske.dal.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
 	
-	private JsonParser parser = new JsonParser();
+	@Autowired
+	private JsonService jsonService;
 	
 	@Autowired
 	private AuthenticationService authenticationService;
 	
-	@RequestMapping(value = {"/connect"}, method = RequestMethod.POST)
+	@RequestMapping(value = {"/connect"}, method = RequestMethod.POST, produces="application/json")
 	public String authenticate(HttpServletRequest request) throws Exception {
-		throw new BadRequestException("Ta gueule");
-//		JsonObject object = parser.parse(request.getReader()).getAsJsonObject();
-//		String email = object.get("email").getAsString();
-//		
-//		
-//		return null;
+		String email = null;
+		String password = null;
+		try {
+			JsonObject object = jsonService.parse(request.getReader()).getAsJsonObject();
+			email = object.get("email").getAsString();
+			password = object.get("password").getAsString();
+		} catch(Exception e) {
+			throw new BadRequestException("Contenu de la requête invalide");
+		}
+		
+		User user = null;
+		JsonObject response = new JsonObject();
+		
+		try {
+			user = authenticationService.authenticate(email, password);
+		} catch (Exception e) {
+			response.addProperty("ok", false);
+			response.addProperty("message", e.getMessage());
+			return jsonService.stringify(response);
+		}
+		
+		try {
+			String token = user.getUserMail() + user.getUserPassword();
+			JsonObject content = new JsonObject();
+			content.addProperty("token", token);
+			response.addProperty("ok", false);
+			response.add("content", content);
+			return jsonService.stringify(response);
+			
+		} catch (Exception e) {
+			response.addProperty("ok", false);
+			response.addProperty("message", e.getMessage());
+			return jsonService.stringify(response);
+		}
 	}
 	
 	
