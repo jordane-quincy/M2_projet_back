7e1a364fcb4dd64773a8c468b23ad94a34fad9f4
Quentin <quentin59131@gmail.com>
2017-06-07 16:41:07 +0200

    Feat(notification):controller

--- SKE/src/main/java/org/istv/ske/core/service/NotificationServiceImpl.java
+++ SKE/src/main/java/org/istv/ske/core/service/NotificationServiceImpl.java
@@ -17,10 +17,9 @@ public class NotificationServiceImpl implements NotificationService {
 	@Autowired
 	NotificationRepository notificationRepository;
 	@Override
-	public Notification createNotification(String title, String content, String type, Date creationDate, User user) {
+	public Notification createNotification(String title, String content, String type, User user) {
 		Notification notification=  new Notification();
 		notification.setContent(content);
-		notification.setCreationDate(creationDate);
 		notification.setTitle(title);
 		notification.setType(type);
 		notification.setUser(user);
@@ -35,10 +34,9 @@ public class NotificationServiceImpl implements NotificationService {
 	}
 
 	@Override
-	public Notification updateNotification( Notification notification, String title, String content,String type, Date creationDate, User user) {
+	public Notification updateNotification( Notification notification, String title, String content,String type, User user) {
 		Notification notificationFound =  notificationRepository.findOne(notification.getId());
 		notificationFound.setContent(content);
-		notificationFound.setCreationDate(creationDate);
 		notificationFound.setTitle(title);
 		notificationFound.setType(type);
 		notificationFound.setUser(user);
--- SKE/src/main/java/org/istv/ske/messages/test/TestClient.java
+++ SKE/src/main/java/org/istv/ske/messages/test/TestClient.java
@@ -1,7 +1,11 @@
 package org.istv.ske.messages.test;
 
+import org.istv.ske.dal.entities.User;
+import org.istv.ske.dal.service.UserService;
 import org.istv.ske.messages.common.EmailClient;
 import org.istv.ske.messages.enums.EmailType;
+import org.istv.ske.messages.enums.TypeNotification;
+import org.istv.ske.messages.manager.NotificationManager;
 import org.istv.ske.messages.model.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,12 @@ public class TestClient {
     @Autowired
     private EmailClient emailClient;
 
+    @Autowired
+    private NotificationManager notificationManager;
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = "/client", method = RequestMethod.GET)
     public void testEmailClient() {
         System.out.println("Debut envoi mail");
@@ -25,5 +35,16 @@ public class TestClient {
         email.init();
         emailClient.sendEmail(email);
         System.out.println("Fin envoi mail");
+    }
+
+    @RequestMapping(value = "/notif", method = RequestMethod.GET)
+    public void testNotif() {
+        System.out.println("Debut creation notif");
+
+        User user = userService.getUser(new Long(1));
+
+        notificationManager.createNotification(user, TypeNotification.SIMPLE);
+
+        System.out.println("Fin creation notif");
     }
 }
