27d5c26faca8a9f52b59166340b723a4e0e15541
Samir <samir.moutawakil59@gmail.com>
2017-06-07 14:16:56 +0200

    MAJ sub

--- SKE/src/main/java/org/istv/ske/dal/repository/AppointmentRepository.java
+++ SKE/src/main/java/org/istv/ske/dal/repository/AppointmentRepository.java
@@ -1,10 +1,14 @@
 package org.istv.ske.dal.repository;
 
+import java.util.List;
+
 import org.istv.ske.dal.entities.Appointment;
+import org.istv.ske.dal.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
 
+	public List<Appointment> findByApplicant(User user);
 }
