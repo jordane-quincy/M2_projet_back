32bd8e0b90bef167f0641a23bc993cb46eeae1ab
MaD <maximdegres@gmail.com>
2017-06-07 09:11:20 +0200

    fix minuscule

--- SKE/src/main/java/org/istv/ske/dal/Appointment.java
+++ SKE/src/main/java/org/istv/ske/dal/Appointment.java
@@ -4,50 +4,46 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import org.springframework.context.annotation.Scope;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 
 @Entity
 @Table(name = "appointment")
 public class Appointment {
-	
 
 	@Id
 	@GeneratedValue
-	@JoinColumn(name="offer_id")
+	@JoinColumn(name = "offer_id")
 	private int appointmentID;
 	private int applicantID;
-	
-	private org.joda.time.DateTime AppointmentDateTime;
-	private String AppointmentStatus;
-	
-	
-	public Appointment(){
-		
+
+	private org.joda.time.DateTime appointmentDateTime;
+	private String appointmentStatus;
+
+	public Appointment() {
+
 	}
 
 	public int getAppointmentID() {
 		return appointmentID;
 	}
+
 	public void setAppointmentID(int appointmentID) {
 		this.appointmentID = appointmentID;
 	}
+
 	public int getApplicantID() {
 		return applicantID;
 	}
+
 	public void setApplicantID(int applicantID) {
 		this.applicantID = applicantID;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Appointment [appointmentID=" + appointmentID + ", applicantID=" + applicantID + "]";
 	}
+
 	public Appointment(int appointmentID, int applicantID) {
 		super();
 		this.appointmentID = appointmentID;
