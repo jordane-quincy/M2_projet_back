380b4ae361afa622b51faa082c5169202da7364d
sarasentouh <sara.tnsi59@gmail.com>
2017-06-07 13:24:27 +0200

    no message

---
+++ SKE/src/test/org/istv/ske/core/controller/SearchControllerTest.java
@@ -0,0 +1,24 @@
+package org.istv.ske.core.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sarasentouh on 07/06/2017.
+ */
+public class SearchControllerTest {
+
+    @Test
+    public void listDomain() throws Exception {
+        // 1 . preparer la requette httpServeletRequest __ front
+        // 2 . appel au WS  __ front
+        // 3 . verfier la reponse  __ fichier json __ json.get("domain").getAsList()
+    }
+
+    @Test
+    public void getSubject() throws Exception {
+    }
+
+}
