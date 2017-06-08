ecfc91eb69683c289bb7a534c12171aad676aee5
Wastamane <duthoit501@gmail.com>
2017-06-08 11:20:39 +0200

    activation acount  coté token

--- SKE/src/main/java/org/istv/ske/core/service/AccountCertificationService.java
+++ SKE/src/main/java/org/istv/ske/core/service/AccountCertificationService.java
@@ -1,9 +1,10 @@
 package org.istv.ske.core.service;
 
+import org.istv.ske.core.exception.BadRequestException;
 import org.istv.ske.dal.entities.User;
 
 
 public interface AccountCertificationService {
-	public User activate(String token);
+	public User activate(String token) throws BadRequestException;
 	public boolean isActivated(long userID);
 }
