050823c66c461623faf07f80b444464b954368df
Samir <samir.moutawakil59@gmail.com>
2017-06-08 14:55:37 +0200

    curse

--- SKE/src/main/java/org/istv/ske/dal/repository/AppointmentRepository.java
+++ SKE/src/main/java/org/istv/ske/dal/repository/AppointmentRepository.java
@@ -3,6 +3,7 @@ package org.istv.ske.dal.repository;
 import java.util.List;
 
 import org.istv.ske.dal.entities.Appointment;
+import org.istv.ske.dal.entities.Appointment.AppointmentStatus;
 import org.istv.ske.dal.entities.Offer;
 import org.istv.ske.dal.entities.User;
 import org.springframework.data.repository.CrudRepository;
@@ -15,5 +16,5 @@ public interface AppointmentRepository extends CrudRepository<Appointment, Long>
 
 	public List<Appointment> findByOfferOrderById(List<Offer> offers);
 
-	public List<Appointment> findByStatusAndApplicant(String status, User user);
+	public List<Appointment> findByStatusAndOffer(AppointmentStatus validated, List<Offer> offers);
 }
