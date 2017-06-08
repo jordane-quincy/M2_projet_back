14051d3e8281d6dcb5578972aefd3f7fac319616
JbRezidu <duriez.jeanbaptiste@gmail.com>
2017-06-08 09:16:44 +0200

    Ajout allow-access-headers

--- SKE/src/main/java/org/istv/ske/configuration/TokenValidationFilter.java
+++ SKE/src/main/java/org/istv/ske/configuration/TokenValidationFilter.java
@@ -29,6 +29,7 @@ public class TokenValidationFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
 		
 		System.out.println(request.getContextPath());
 		if(request.getServletPath().equals("/auth/connect") || request.getServletPath().equals("/user/create")) {
