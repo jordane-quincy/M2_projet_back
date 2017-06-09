f17fba5a2913f0e8e642703901a6e751a6fdd3a8
clemscode <ruffin.cle@gmail.com>
2017-06-09 09:29:17 +0200

    NotificationController

---
+++ SKE/src/main/java/org/istv/ske/core/controller/NotificationController.java
@@ -0,0 +1,45 @@
+package org.istv.ske.core.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.istv.ske.core.service.NotificationService;
+import org.istv.ske.core.service.UserService;
+import org.istv.ske.dal.entities.Notification;
+import org.istv.ske.dal.entities.User;
+import org.istv.ske.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+
+	@Autowired
+	private TokenService tokenService;
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private NotificationService notificationService;
+
+	@RequestMapping(value = { "/list" }, method = RequestMethod.GET, produces = "application/json")
+	public List<Notification> list(HttpServletRequest request) throws Exception {
+		Long userId = tokenService.getUserIdByToken(request);
+		User user = userService.getUser(userId);
+		return notificationService.findNotificationByUser(user);
+	}
+
+	@RequestMapping(value = { "/markasread/{id}" }, method = RequestMethod.POST, produces = "application/json")
+	public String markAsRead(HttpServletRequest request, @PathVariable(required = true) Long notificationId)
+			throws Exception {
+		Long userId = tokenService.getUserIdByToken(request);
+
+	}
+
+}
