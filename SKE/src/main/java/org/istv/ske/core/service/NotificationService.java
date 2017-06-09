d86dfd8e7f4206e93c336afacdcd4a4759839d77
clemscode <ruffin.cle@gmail.com>
2017-06-09 15:03:32 +0200

    #

--- SKE/src/main/java/org/istv/ske/core/service/NotificationService.java
+++ SKE/src/main/java/org/istv/ske/core/service/NotificationService.java
@@ -1,6 +1,5 @@
 package org.istv.ske.core.service;
 
-import java.util.Date;
 import java.util.List;
 
 import org.istv.ske.dal.entities.Notification;
@@ -8,18 +7,19 @@ import org.istv.ske.dal.entities.User;
 
 public interface NotificationService {
 
-    Notification createNotification(String title, String content, Notification.NotificationType type, User user);
+	Notification createNotification(String title, String content, Notification.NotificationType type, User user);
 
-    void deleteNotification(Notification notification);
+	void deleteNotification(Notification notification);
 
-    Notification updateNotification(Notification notification, String title, String content, Notification.NotificationType type, User user);
+	Notification updateNotification(Notification notification, String title, String content,
+			Notification.NotificationType type, User user);
 
-    List<Notification> getAll();
+	List<Notification> getAll();
 
-    Notification findNotificationById(Long id);
+	Notification findNotificationById(Long id);
 
-    List<Notification> findNotificationByUserByOrderByCreationDateDesc(User user);
+	List<Notification> findNotificationByUserByOrderByCreationDateDesc(User user);
 
-    Notification asRead(Long notificationID);
+	Notification asRead(Long notificationID);
 
 }
