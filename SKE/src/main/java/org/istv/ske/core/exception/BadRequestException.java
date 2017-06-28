4cedd51d3553094530b80f70409a038a0805b45b
JordanCanonne <jordan.canonne@etu.univ-valenciennes.fr>
2017-06-28 11:08:38 +0200

    Commentaire sur les classes Core/Exception

--- SKE/src/main/java/org/istv/ske/core/exception/BadRequestException.java
+++ SKE/src/main/java/org/istv/ske/core/exception/BadRequestException.java
@@ -1,13 +1,16 @@
 package org.istv.ske.core.exception;
 
+// Classe qui définit l'exeption à retourner au client lorsqu'une erreur est survenu côté Front
 public class BadRequestException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+	// Message a afficher au client
 	public BadRequestException(String message) {
 		super(message);
 	}
 	
+	// Code erreur 500 lorsque l'erreur vient du Front
 	public int getStatus() {
 		return 400;
 	}
--- SKE/src/main/java/org/istv/ske/core/exception/InternalException.java
+++ SKE/src/main/java/org/istv/ske/core/exception/InternalException.java
@@ -1,13 +1,16 @@
 package org.istv.ske.core.exception;
 
+// Classe qui définit l'exeption à retourner au client lorsqu'une erreur est survenu côté Back
 public class InternalException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 
+	// Message a afficher au client
 	public InternalException(String message) {
 		super(message);
 	}
 	
+	// Code erreur 500 lorsque l'erreur vient du Back
 	public int getStatus() {
 		return 500;
 	}
